7f3a7131a648ee9e6d6866e0079954ad83417cde
Shinren Pan <shinren.pan@gmail.com>
2023-08-23 15:26:43 +0800

    fixed img uri encode/decode 問題

--- Sources/Reader/Views/Reader.Views.Cell.swift
+++ Sources/Reader/Views/Reader.Views.Cell.swift
@@ -63,7 +63,13 @@ private extension Reader.Views.Cell {
     }
     
     func makeRequest(uri: String) -> URLRequest? {
-        guard let uri = uri.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+        // 先 decode 再 encode
+        // 因為有些網址很奇怪, ex:
+        // https://i.hamreus.com/ps3/g/gjszstgtl_xzh/第22.1话/0%20%281%29%20%E6%8B%B7%E8%B4%9D.jpg.webp
+        // 話數沒 encode, 但是圖片有 encode
+        // 所以都先 decode 變成 https://i.hamreus.com/ps3/g/gjszstgtl_xzh/第22.1话/0 (1) 拷贝.jpg.webp
+        // 再 encode 成 https://i.hamreus.com/ps3/g/gjszstgtl_xzh/%E7%AC%AC22.1%E8%AF%9D/0%20(1)%20%E6%8B%B7%E8%B4%9D.jpg.webp
+        guard let uri = uri.removingPercentEncoding?.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
             return nil
         }
         
